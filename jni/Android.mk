469c88ea5e3e3d23758fb2d24a0d280f78457fb4
Haris Khaliq <hkhaliq@bionym.com>
2015-10-28 17:46:04 -0400

    Update android makefile

--- jni/Android.mk
+++ jni/Android.mk
@@ -14,38 +14,46 @@ BOOST_ROOT := $(INCLUDE_PATH)/../boost
 endif
 
 THRIFT_SRC_FILES := \
-    $(wildcard $(SRC_DIR)/*.cpp) \
-    $(wildcard $(SRC_DIR)/async/*.cpp) \
-    #$(SRC_PATH)/async/TAsyncChannel.cpp \
-    #$(SRC_PATH)/async/TAsyncProtocolProcessor.cpp \
+    $(SRC_PATH)/Thrift.cpp \
+    $(SRC_PATH)/TOutput.cpp \
+    $(SRC_PATH)/VirtualProfiling.cpp \
+    $(SRC_PATH)/TApplicationException.cpp \
+    $(SRC_PATH)/async/TAsyncChannel.cpp \
+    $(SRC_PATH)/async/TAsyncProtocolProcessor.cpp \
+    $(SRC_PATH)/async/TConcurrentClientSyncInfo.cpp \
     $(SRC_PATH)/processor/PeekProcessor.cpp \
-    $(SRC_PATH)/transport/TSocket.cpp \
-    $(SRC_PATH)/transport/TTransportException.cpp \
-    $(SRC_PATH)/transport/TZlibTransport.cpp \
-    $(SRC_PATH)/transport/TSocketPool.cpp \
-    $(SRC_PATH)/transport/TServerSocket.cpp \
-    $(SRC_PATH)/transport/THttpClient.cpp \
-    $(SRC_PATH)/transport/TTransportUtils.cpp \
-    $(SRC_PATH)/transport/THttpTransport.cpp \
     $(SRC_PATH)/transport/TBufferTransports.cpp \
-    $(SRC_PATH)/transport/TFileTransport.cpp \
-    $(SRC_PATH)/transport/TSimpleFileTransport.cpp \
-    $(SRC_PATH)/transport/THttpServer.cpp \
     $(SRC_PATH)/transport/TFDTransport.cpp \
-    $(SRC_PATH)/concurrency/Util.cpp \
-    $(SRC_PATH)/concurrency/PosixThreadFactory.cpp \
-    $(SRC_PATH)/concurrency/TimerManager.cpp \
-    $(SRC_PATH)/concurrency/Mutex.cpp \
-    $(SRC_PATH)/concurrency/ThreadManager.cpp \
+    $(SRC_PATH)/transport/TFileTransport.cpp \
+    $(SRC_PATH)/transport/THttpClient.cpp \
+    $(SRC_PATH)/transport/THttpServer.cpp \
+    $(SRC_PATH)/transport/TPipe.cpp \
+    $(SRC_PATH)/transport/TPipeServer.cpp \
+    $(SRC_PATH)/transport/TServerSocket.cpp \
+    $(SRC_PATH)/transport/TSimpleFileTransport.cpp \
+    $(SRC_PATH)/transport/TSocket.cpp \
+    $(SRC_PATH)/transport/TSocketPool.cpp \
+    $(SRC_PATH)/transport/TTransportException.cpp \
+    $(SRC_PATH)/transport/TTransportUtils.cpp \
+    $(SRC_PATH)/transport/TZlibTransport.cpp \
+    $(SRC_PATH)/concurrency/BoostMonitor.cpp \
+    $(SRC_PATH)/concurrency/BoostMutex.cpp \
+    $(SRC_PATH)/concurrency/BoostThreadFactory.cpp \
     $(SRC_PATH)/concurrency/Monitor.cpp \
-    $(SRC_PATH)/server/TThreadPoolServer.cpp \
+    $(SRC_PATH)/concurrency/Mutex.cpp \
+    $(SRC_PATH)/concurrency/PosixThreadFactory.cpp \
+    $(SRC_PATH)/concurrency/StdMonitor.cpp \
+    $(SRC_PATH)/concurrency/StdMutex.cpp \
+    $(SRC_PATH)/concurrency/StdThreadFactory.cpp \
+    $(SRC_PATH)/concurrency/ThreadManager.cpp \
+    $(SRC_PATH)/concurrency/TimerManager.cpp \
+    $(SRC_PATH)/concurrency/Util.cpp \
+    $(SRC_PATH)/server/TConnectedClient.cpp \
     $(SRC_PATH)/server/TServer.cpp \
-    $(SRC_PATH)/server/TThreadedServer.cpp \
+    $(SRC_PATH)/server/TServerFramework.cpp \
     $(SRC_PATH)/server/TSimpleServer.cpp \
-    $(SRC_PATH)/protocol/TDebugProtocol.cpp \
-    $(SRC_PATH)/protocol/TDenseProtocol.cpp \
-    $(SRC_PATH)/protocol/TJSONProtocol.cpp \
-    $(SRC_PATH)/protocol/TBase64Utils.cpp \
+    $(SRC_PATH)/server/TThreadedServer.cpp \
+    $(SRC_PATH)/server/TThreadPoolServer.cpp
 
     # not supported yet
     # async/TEvhttpServer.cpp \
